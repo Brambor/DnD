96f813a771a53ff39342b1dcca7674ffdc0caac7
Brambor <tondadrd@seznam.cz>
2019-06-10 17:43:34 +0200

    some more test handling & fixes
    tests
    - fixed that n_of_lines_read didn't restart between all but the first test to end early or not playing out at all
    - changed sleep constanst (between tests 2s -> 1s; between line read 0.25s -> 0.1s)
    - errors (EOF, others) are now print to "fight" window, so not only to underlying pure-text console (not visible)
    - after test is done, pretty much normal enviorement is restored with the last test taken its effect (entities and stuff are left there, not deleted, not restarted)

--- DnD.py
+++ DnD.py
@@ -67,7 +67,8 @@ else:
 	cPrint = CustomPrint(windows, curses)
 	cInput = CustomInput(cPrint, cCurses, input_stream=True, test_environment=True)  # input_stream latter changed 
 
-	for test in ("basics",): #("basics cast dead_splash dmg effect_stats fight freeze freeze_fire freeze_hard help").split():
+	for test in ("basics cast dead_splash dmg effect_stats fight freeze freeze_fire freeze_hard help").split():
+		cInput.i = 0
 		path = 'tests/test_%s.txt' % test
 		f = open(path,'r')
 		f_copy = open(path,'r').read().split("\n")
@@ -80,16 +81,20 @@ else:
 		try:
 			windows["fight"].addstr("test name: %s" % test)
 			windows["fight"].refresh()
-			sleep(2)
+			sleep(1)
 			while cInput.i+1 < len(f_copy):
 				command = cInput(">>>")
 				P.process(command)
-				sleep(0.25)
+				sleep(0.1)
 			windows["fight"].addstr("\n\n\n\n")
 			windows["fight"].refresh()
 		except EOFError:
 			print("EOF happened")
+			windows["fight"].addstr("ERROR EOF happened")
+			windows["fight"].refresh()
 		except Exception as e:
+			windows["fight"].addstr("ERROR Exception happened")
+			windows["fight"].refresh()
 			cCurses.endCurses()
 			print("\n\n")
 			print(e)
@@ -98,7 +103,29 @@ else:
 			raise
 
 	f.close()
-
+	windows["fight"].addstr("TESTS ARE DONE")
+	windows["fight"].refresh()
 	sys.stdin = f1
+
+	cInput.test_environment = False
+	cInput.input_stream = False
+
+	try:
+		while True:
+			command = cInput(">>>")
+			if command == "&" and settings.DEBUG:
+				crash  # force crash
+			if command == "exit":
+				break  # force crash
+
+			P.process(command)
+	except Exception as e:
+		cCurses.endCurses()
+		print("\n\n")
+		print(e)
+		input("CRASHED, PRESS ENTER")
+		raise
+
+
 	cCurses.endCurses()
 	input("DONE (settings.AUTO_INPUT was set to True)")
