7f7e2f5ca9119e2dba450eab653910ee2ce06f77
Brambor <tondadrd@seznam.cz>
2019-10-25 00:30:32 +0200

    added dice d14 d16 d18 and rebalanced crit

--- modules/Dice.py
+++ modules/Dice.py
@@ -3,32 +3,36 @@ from modules.DnDException import DnDException
 
 
 def dice_stat(n):
-	if n >= 5:
+	if n >= 8:
 		return D(20)
 	elif n < 0:
 		return D(4)
-	return D([4, 6, 8, 10, 12][n])
+	return D(list(all_dice)[n])
 
 def D(n):
 	return randint(1, n)
 
-def dice_which(n):
-	if n not in all_dice:
-		raise DnDException("Dice %d doesn't exist. Existing die: %s." % (n, ", ".join(str(k) for k in all_dice.keys())))
-	return all_dice[n]
-
 def dice_crit(dice, threw, cPrint):
-	if dice - threw <= dice_which(dice):
+	check_dice_exists(dice)
+	if threw >= all_dice[dice]:
 		cPrint("Critical on D%d!!\n" % dice)
 		return True
 	else:
 		return False
 
+def check_dice_exists(n):
+	if n not in all_dice:
+		raise DnDException("Dice %d doesn't exist. Existing die: %s." % (n, ", ".join(str(d) for d in all_dice)))
+
+# dice : since which n does it crit
 all_dice = {
-	4: 0,
-	6: 1,
-	8: 2,
-	10: 3,
-	12: 4,
-	20: 5,
+	4: 4,
+	6: 5,
+	8: 7,
+	10: 8,
+	12: 10,
+	14: 11,
+	16: 13,
+	18: 14,
+	20: 15,
 }
