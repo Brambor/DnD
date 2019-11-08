fe450471fd1549e3f51609e5a972cac3e1a2a612
Brambor <tondadrd@seznam.cz>
2019-11-08 21:51:22 +0100

    DnD.py cleanup using wrappers

--- DnD.py
+++ DnD.py
@@ -23,34 +23,60 @@ class Effect():
 		pass
 """
 
-tests = [f[5:-4] for f in os.listdir("tests")]
-print("Avaiable tests: %s" % ", ".join(tests))
-if not settings.AUTO_INPUT:
-	do_tests = False
-while settings.AUTO_INPUT:
-	the_input = input("Press enter if you wish to proceed.\nWrite '[t]est' to run all tests. Write 'test_name*' to select which tests to run.\n'*' is a symbol\n>>>")
-	do_tests = False
-	if the_input == "":
-		break
-	elif the_input in ("test", "t"):
-		do_tests = True
-		break
-	else:
-		do_tests = True
-		tests_selected = the_input.split()
-		good = True
-		for t in tests_selected:
-			if t not in tests:
-				print("'%s' is not a valid test." % t)
-				good = False
-		if not good:
-			continue
-		tests = tests_selected
-		break
+def wrapper(func, stdin=None, **kwargs):
+	try:
+		func(**kwargs)
+	except EOFError:
+		print("EOF happened")
+		cPrint("ERROR EOF happened\n")
+	except Exception as e:
+		cPrint("ERROR Exception happened\n")
+		cCurses.endCurses()
+		print("\n\n")
+		if stdin:
+			sys.stdin = stdin
+		traceback.print_exc(file=sys.stdout)
+		input("CRASHED, PRESS ENTER")
+		raise
+
+def regular_wrap():
+	while P.input_command():
+		pass
+
+def test_wrap(test, len_f_copy):
+	cPrint("test name: %s\n" % test)
+	sleep(1)
+	while (cInput.i+1 < len_f_copy) and P.input_command():
+		sleep(0.1)
+	cPrint("\n\n\n\n")
+
+do_tests = False
+if settings.AUTO_INPUT:
+	tests = [f[5:-4] for f in os.listdir("tests")]
+	print("Avaiable tests: %s" % ", ".join(tests))
+
+	while True:
+		the_input = input("Press enter if you wish to proceed.\nWrite '[t]est' to run all tests. Write 'test_name*' to select which tests to run.\n'*' is a symbol\n>>> ")
+		if the_input == "":
+			break
+		elif the_input in ("test", "t"):
+			do_tests = True
+			break
+		else:
+			do_tests = True
+			tests_selected = the_input.split()
+			good = True
+			for t in tests_selected:
+				if t not in tests:
+					print("'%s' is not a valid test." % t)
+					good = False
+			if not good:
+				continue
+			tests = tests_selected
+			break
 
 if not do_tests:
 	# REGULAR USE
-
 	if settings.LOG:
 		import datetime
 		current_time = str(datetime.datetime.today()).split(".")[0]
@@ -66,19 +92,11 @@ if not do_tests:
 
 	G = Game(library, cPrint, cCurses)
 	P = Parser(G, cInput, cPrint, settings.DEBUG)
-	try:
-		while P.input_command():
-			pass
-	except Exception as e:
-		cCurses.endCurses()
-		print("\n\n")
-		print(e)
-		traceback.print_exc(file=sys.stdout)
-		input("CRASHED, PRESS ENTER")
-		raise
+	
+	wrapper(regular_wrap)
+
 	cCurses.endCurses()
 	input("You are exiting")
-
 else:
 	# TESTING
 	import sys
@@ -103,42 +121,18 @@ else:
 		P = Parser(G, cInput, cPrint, settings.DEBUG)
 		sys.stdin = f
 
-		try:
-			cPrint("test name: %s\n" % test)
-			sleep(1)
-			while (cInput.i+1 < len(f_copy)) and P.input_command():
-				sleep(0.1)
-			cPrint("\n\n\n\n")
-		except EOFError:
-			print("EOF happened")
-			cPrint("ERROR EOF happened\n")
-		except Exception as e:
-			cPrint("ERROR Exception happened\n")
-			cCurses.endCurses()
-			print("\n\n")
-			print(e)
-			sys.stdin = f1
-			traceback.print_exc(file=sys.stdout)
-			input("CRASHED, PRESS ENTER")
-			raise
+		wrapper(test_wrap, stdin=f1, test=test, len_f_copy=len(f_copy))
 
+		f.close()
 	f.close()
+
 	cPrint("TESTS ARE DONE\n")
 	sys.stdin = f1
 
 	cInput.test_environment = False
 	cInput.input_stream = False
 
-	try:
-		while P.input_command():
-			pass
-	except Exception as e:
-		cCurses.endCurses()
-		print("\n\n")
-		print(e)
-		traceback.print_exc(file=sys.stdout)
-		input("CRASHED, PRESS ENTER")
-		raise
+	wrapper(regular_wrap)
 
 	cCurses.endCurses()
-	input("DONE (settings.AUTO_INPUT was set to True)")
+	input("You are exiting")
