5d94484bf9c19c0c09c625abe4d8f644c5ea721a
Brambor <tondadrd@seznam.cz>
2019-11-27 16:00:42 +0100

    Created entities_local_example.py

---
+++ library/entities_local_example.py
@@ -0,0 +1,36 @@
+"""
+This is an example of how to use 'library/*_local.py'.
+You can copy this file and remove '_example' at the end making it 'entities_local.py'
+After that, when you launch DnD.py, it should say:
+	'''
+	'entities' loaded
+	        'weaping_angel' overrode
+	'''
+at the begining of the output.
+"""
+
+
+from modules.Weapons import Fist, Axe
+
+"""
+DOCS: Notify the programmer that you wanted to make entities, but they didn't write the docs...
+"""
+
+entities = {
+	"weaping_angel": {
+		"nickname": "weaping_angel",
+		"hp_max": 3,
+	},
+	"wolf": {
+		"nickname": "wolf",
+		"hp_max": 15,
+	},
+}
+
+#entities["pes"]["weapon"]["dmg"] = 2
+#entities["pes"]["weapon"]["dice"] = "d8"
+
+
+# Add names to spells
+for e in entities:
+	entities[e]["derived_from"] = e
