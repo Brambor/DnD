19a83d3f011eeffccb2f01d36c181da841338802
Brambor <tondadrd@seznam.cz>
2019-11-29 03:37:07 +0100

    added Warning: deprecated settings

--- modules/SettingsLoader.py
+++ modules/SettingsLoader.py
@@ -6,6 +6,12 @@ try:
 except ImportError:
 	print("Custom settings not loaded. Look into settings/settings_default.py to see how to make it.")
 
+deprecated = (set(dir(settings_local)) - set(dir(settings)))
+if deprecated:
+	print(
+		"Warning! These settings in settings/settings_local.py are deprecated"
+		" <==> do nothing. You should delete them:\n\t%s\n" % "\n\t".join(deprecated))
+
 for value in dir(settings_local):
 	if not value.startswith("__"):
 		setattr(settings, value, getattr(settings_local, value))
