88f0b67df3e2ff5decf19e0f6891bf095c7c1303
Brambor <tondadrd@seznam.cz>
2020-04-05 22:59:47 +0200

    added aliases to damage_types

--- library/damage_types.py
+++ library/damage_types.py
@@ -23,4 +23,44 @@ damage_types = {
 
 damage_types = dict(((d, d) for d in damage_types))
 
+for alias, original in (
+	("a", "acid"),
+	("b", "blunt"),
+	("e", "elemental"),
+	("ele", "elemental"),
+	("f", "fire"),
+	("i", "ice"),
+	("c", "ice"),
+	("cold", "ice"),
+	("cool", "ice"),
+	("l", "lightning"),
+	("lig", "lightning"),
+	("light", "lightning"),
+	("m", "magic"),
+	("mag", "magic"),
+	("n", "necrotic"),
+	("nec", "necrotic"),
+	("necro", "necrotic"),
+	("p", "physical"),
+	("phys", "physical"),
+	("pie", "piercing"),
+	("pierce", "piercing"),
+	("poi", "poison"),
+	("psy", "psychic"),
+	("psych", "psychic"),
+	("r", "radiant"),
+	("rad", "radiant"),
+	("s", "slashing"),
+	("slash", "slashing"),
+	("blade", "slashing"),
+	("t", "true"),
+	("w", "water"),
+	("wat", "water"),
+	("wi", "wind"),
+	("air", "wind"),
+):
+	if alias in damage_types:
+		raise ValueError("damage_types already contains %s." % alias)
+	damage_types[alias] = original
+
 output = local_loader(damage_types, "library.damage_types_local", "damage_types")
