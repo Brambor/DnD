ebe57ffe86ede53e49e0bb6bb288cc72c2957fcc
Brambor <tondadrd@seznam.cz>
2020-08-03 14:56:18 +0200

    Misc.calculate replace("%", "/100")

--- modules/Misc.py
+++ modules/Misc.py
@@ -6,6 +6,7 @@ from modules.Dice import D
 
 
 def calculate(string):
+	string = string.replace("%", "/100")
 	unallowed = set(string) - set("0123456789-+*/.() ")
 	if unallowed:
 		raise DnDException(f"These character(s) '{''.join(unallowed)}' are not allowed in calculation.")
